84b29bf4c27dff3d0d538870eb22a34bb08ac9b1
lebenlechzer <lebenlechzer@gmail.com>
2017-04-02 18:09:38 +0200

    Better login handling

--- crawler/crawler/spiders/linkedin_spider.py
+++ crawler/crawler/spiders/linkedin_spider.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python
 import scrapy
+import base64
 
 class LinkedinSpider(scrapy.Spider):
     name = "linkedin"
+    login_url = 'https://www.linkedin.com/'
     start_urls = [
-        'https://www.linkedin.com/'
+        'https://www.linkedin.com/directory/people-a-33-13-13/'
     ]
 
-    def parse(self, response):
-        login_form = response.css('.login-form').extract_first()
-        return scrapy.FormRequest.from_response(
-            response,
-            formdata = {'session_key': 'test.test@test.com',
-                        'session_password': '123456'},
-            callback = self.after_login
-        )
+    def start_requests(self):
+        yield scrapy.Request(url=self.login_url, callback=self.login)
 
-    def after_login(self, response):
-        # check login succeed before going on
+    def login(self, response):
+        with open("cred.txt") as f:
+            email = base64.b64decode(f.readline())
+            password = base64.b64decode(f.readline())
+        return scrapy.FormRequest.from_response(
+            response, formdata={'session_key': email,
+                                'session_password': password},
+            callback=self.check_login_response)
+
+    def check_login_response(self, response):
         if b'login-error' in response.body:
             self.logger.error("Login failed")
             return
         else:
             self.logger.info("Login successful")
+            for u in self.start_urls:
+                yield scrapy.Request(u, callback=self.parse_member_dir)
+
+    def parse_member_dir(self, response):
+        self.logger.info("Got data")
